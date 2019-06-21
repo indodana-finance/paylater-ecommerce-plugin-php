0d348bdfdbd0d3d3278f5a84fe386b6477859222
Gery Wahyu <gwahyu@gmail.com>
2019-06-21 13:51:27 +0700

    Remove unnecessary cache files and remap port

--- cli/db/blueprint/mysql/prestashop/__migrations__/public/2019.06.11.0001__initialize__prestashop.sql
+++ cli/db/blueprint/mysql/prestashop/__migrations__/public/2019.06.11.0001__initialize__prestashop.sql
@@ -6187,7 +6187,7 @@ CREATE TABLE `ps_shop_url` (
 
 LOCK TABLES `ps_shop_url` WRITE;
 /*!40000 ALTER TABLE `ps_shop_url` DISABLE KEYS */;
-INSERT INTO `ps_shop_url` VALUES (1,1,'localhost:9003','localhost:9003','/','',1,1);
+INSERT INTO `ps_shop_url` VALUES (1,1,'localhost:6104','localhost:6104','/','',1,1);
 /*!40000 ALTER TABLE `ps_shop_url` ENABLE KEYS */;
 UNLOCK TABLES;
 
