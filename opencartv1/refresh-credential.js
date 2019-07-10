0d973024cfdb0f7f0799673d18fe2157d387886d
Gery Wahyu <gwahyu@gmail.com>
2019-07-10 17:07:08 +0700

    Clean credential refresh codes

--- opencartv1/refresh-credential.js
+++ opencartv1/refresh-credential.js
@@ -8,20 +8,10 @@ dotenv.config({
   path: "../../../cli/NAMESPACE"
 });
 
-const HOST = process.env.HOST;
-const PORT = process.env.PORT;
-
 const ORGANIZATION = process.env.ORGANIZATION;
 const TEAM = process.env.TEAM;
 const PRODUCT = process.env.PRODUCT;
 const ENVIRONMENT = process.env.ENVIRONMENT;
-
-const CONFIG_RELATIVE_PATH = "upload/config.php";
-const ADMIN_CONFIG_RELATIVE_PATH = "upload/admin/config.php";
-
-const CACERT_FILE_PATH = `/usr/share/pki/certs/service/pios-${ENVIRONMENT}/pios-${ENVIRONMENT}.ca.crt`;
-const CERT_FILE_PATH = `/usr/share/pki/certs/service/pios-${ENVIRONMENT}/pios-${ENVIRONMENT}.crt`;
-const KEY_FILE_PATH = `/usr/share/pki/certs/service/pios-${ENVIRONMENT}/.private/pios-${ENVIRONMENT}.key`;
 
 function getVaultBaseUrl() {
   const dbctlConfigFilePath = `../../../cli/db/blueprint/mysql/opencartv1/${ENVIRONMENT}.yml`;
@@ -30,13 +20,11 @@ function getVaultBaseUrl() {
   return dbctlConfig.secrets.vault.host;
 }
 
-async function getToken(name) {
+async function getToken(name, certificates) {
   const vaultBaseUrl = getVaultBaseUrl();
   const options = {
+    ...certificates,
     url: `${vaultBaseUrl}/v1/auth/cert/login`,
-    cert: fs.readFileSync(CERT_FILE_PATH),
-    key: fs.readFileSync(KEY_FILE_PATH),
-    ca: fs.readFileSync(CACERT_FILE_PATH),
     form: JSON.stringify({
       "name": name
     })
@@ -49,14 +37,12 @@ function getCredentialEndpoint(role) {
   return `v1.1/${ORGANIZATION}/${TEAM}/db/hosted/mysql/${PRODUCT}/opencartv1/${ENVIRONMENT}/creds/${role}`;
 }
 
-async function getCredential(token, role) {
+async function getCredential(token, role, certificates) {
   const vaultBaseUrl = getVaultBaseUrl();
   const endpoint = getCredentialEndpoint(role);
   const options = {
+    ...certificates,
     url: `${vaultBaseUrl}/v1/${endpoint}`,
-    cert: fs.readFileSync(CERT_FILE_PATH),
-    key: fs.readFileSync(KEY_FILE_PATH),
-    ca: fs.readFileSync(CACERT_FILE_PATH),
     headers: {
       "X-Vault-Token": token
     }
@@ -66,17 +52,28 @@ async function getCredential(token, role) {
 }
 
 async function readCredential() {
+  const servicesCertificatePath = `/usr/share/pki/certs/service`
+  const cacertFilePath = `${servicesCertificatePath}/pios-${ENVIRONMENT}/pios-${ENVIRONMENT}.ca.crt`;
+  const certFilePath = `${servicesCertificatePath}/pios-${ENVIRONMENT}/pios-${ENVIRONMENT}.crt`;
+  const keyFilePath = `${servicesCertificatePath}/pios-${ENVIRONMENT}/.private/pios-${ENVIRONMENT}.key`;
+
+  const certificates = {
+    ca: fs.readFileSync(cacertFilePath),
+    cert: fs.readFileSync(certFilePath),
+    key: fs.readFileSync(keyFilePath)
+  }
+
   const name = `${ORGANIZATION}-${TEAM}-${PRODUCT}-${ENVIRONMENT}`;
   const role = "app";
 
   let token = "";
   try {
-    token = await getToken(name);
+    token = await getToken(name, certificates);
   } catch (error) {
     console.log("Unable to get token", error.message);
   }
   try {
-    const credentials = await getCredential(token, role);
+    const credentials = await getCredential(token, role, certificates);
     return credentials;
   } catch (error) {
     console.log("Unable to get credentials", error.message);
@@ -89,8 +86,8 @@ function writeCredentialToTemplate(username, password, inputPath, outputPath) {
   const result = nunjucks.render(inputPath, {
     db_username: username,
     db_password: password,
-    host: HOST,
-    port: PORT
+    host: process.env.HOST,
+    port: process.env.PORT
   });
   const outputFd = fs.openSync(outputPath, "w");
   fs.writeSync(outputFd, result);
@@ -99,14 +96,28 @@ function writeCredentialToTemplate(username, password, inputPath, outputPath) {
 
 async function refreshCredential(currentTimestamp) {
   const credentials = await readCredential();
-  
   if (credentials === null) {
     console.log("Failure happens in one of our processes, Aborting...");
     return;
   }
 
-  writeCredentialToTemplate(credentials.data.username, credentials.data.password, "config.njk", CONFIG_RELATIVE_PATH);
-  writeCredentialToTemplate(credentials.data.username, credentials.data.password, "admin.config.njk", ADMIN_CONFIG_RELATIVE_PATH);
+  const configTemplatePath = "config.njk";
+  const configPath = "upload/config.php";
+  const adminConfigTemplatePath = "admin.config.njk";
+  const adminConfigPath = "upload/admin/config.php";
+
+  writeCredentialToTemplate(
+    credentials.data.username, 
+    credentials.data.password, 
+    configTemplatePath, 
+    configPath
+  );
+  writeCredentialToTemplate(
+    credentials.data.username, 
+    credentials.data.password, 
+    adminConfigTemplatePath, 
+    adminConfigPath
+  );
 
   refreshTimestamp = currentTimestamp + credentials.lease_duration * 1000;
   console.log("Next Credential Refresh: " + new Date(refreshTimestamp));
