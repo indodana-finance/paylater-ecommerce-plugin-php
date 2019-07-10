41e4674b56daf6cedc8ee096a13445aeaeef8b71
Antony Halim <antony@indodana.com>
2019-07-10 13:48:57 +0700

    Tidy up sv

--- cli/sv/src/handler/_parser/__init__.py
+++
@@ -1,3 +0,0 @@
-from .parser import Parser
-from .positional_parser import PositionalParser
-from .argparse_parser import ArgparseParser
--- cli/sv/src/pyinstaller_hooks/hook-template.py
+++
@@ -1,5 +0,0 @@
-from PyInstaller.utils.hooks import collect_data_files
-
-# This is required because the `template` directory contains static files that
-# are not directly imported by Python.
-datas = collect_data_files('template', include_py_files=True)
