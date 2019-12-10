a36ad8ca32701719ffda0780d67373a1620145d5
Antony Halim <antony@indodana.com>
2019-12-10 16:28:40 +0700

    Add DBCTL latest build

--- cli/db/blueprint/postgres/templatedb/__migrations__/public/2017.07.11.0001__initialize_templatedb.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE sample (
-    id character varying NOT NULL,
-    is_active boolean DEFAULT false,
-    created_at timestamp without time zone DEFAULT now() NOT NULL,
-    created_by character varying DEFAULT 'postgres'::character varying NOT NULL,
-    last_modified_at timestamp without time zone DEFAULT now() NOT NULL,
-    last_modified_by character varying DEFAULT 'postgres'::character varying NOT NULL
-);
