a65295f8da4f1a85397af56cde3952f6244b5521
Sendy Halim <sendyhalim93@gmail.com>
2019-12-19 17:06:22 +0700

    Update build and run script to use built-in PHP server

--- opencartv1/upload/config.php
+++ opencartv1/upload/config.php
@@ -8,17 +8,22 @@ define('HTTP_ADMIN', 'admin/');
 define('HTTPS_SERVER', '');
 define('HTTPS_IMAGE', 'image/');
 
+// Our own custom function
+function opencart_upload_path($path) {
+    return __DIR__ . $path;
+}
+
 // DIR
-define('DIR_APPLICATION', '/home/opencart/opencart-1.5.6.4/upload/catalog/');
-define('DIR_SYSTEM', '/home/opencart/opencart-1.5.6.4/upload/system/');
-define('DIR_DATABASE', '/home/opencart/opencart-1.5.6.4/upload/system/database/');
-define('DIR_LANGUAGE', '/home/opencart/opencart-1.5.6.4/upload/catalog/language/');
-define('DIR_TEMPLATE', '/home/opencart/opencart-1.5.6.4/upload/catalog/view/theme/');
-define('DIR_CONFIG', '/home/opencart/opencart-1.5.6.4/upload/system/config/');
-define('DIR_IMAGE', '/home/opencart/opencart-1.5.6.4/upload/image/');
-define('DIR_CACHE', '/home/opencart/opencart-1.5.6.4/upload/system/cache/');
-define('DIR_DOWNLOAD', '/home/opencart/opencart-1.5.6.4/upload/download/');
-define('DIR_LOGS', '/home/opencart/opencart-1.5.6.4/upload/system/logs/');
+define('DIR_APPLICATION', opencart_upload_path('/catalog/'));
+define('DIR_SYSTEM', opencart_upload_path('/system/'));
+define('DIR_DATABASE', opencart_upload_path('/system/database/'));
+define('DIR_LANGUAGE', opencart_upload_path('/catalog/language/'));
+define('DIR_TEMPLATE', opencart_upload_path('/catalog/view/theme/'));
+define('DIR_CONFIG', opencart_upload_path('/system/config/'));
+define('DIR_IMAGE', opencart_upload_path('/image/'));
+define('DIR_CACHE', opencart_upload_path('/system/cache/'));
+define('DIR_DOWNLOAD', opencart_upload_path('/download/'));
+define('DIR_LOGS', opencart_upload_path('/system/logs/'));
 
 // DB
 define('DB_DRIVER', 'mysql');
