5964de9550a95e98c116a6573b86cd6f5a97b647
Sendy Halim <sendyhalim93@gmail.com>
2020-01-22 18:53:59 +0700

    Setup sentry

--- woocommerce/plugin/wp-content/plugins/indodana-payment/library/Indodana/Payment/autoload.php
+++ woocommerce/plugin/wp-content/plugins/indodana-payment/library/Indodana/Payment/autoload.php
@@ -4,6 +4,8 @@ require_once(__DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'a
 define('INDODANA_LIB_DIR', __DIR__ . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR);
 define('INDODANA_LOG_DIR', __DIR__ . DIRECTORY_SEPARATOR . 'log' . DIRECTORY_SEPARATOR);
 
+Sentry\init(['dsn' => 'https://aaaf792679a149128b5a2cf1441684b4@sentry.io/1892625' ]);
+
 spl_autoload_register(function($className) {
     static $classMap;
 
