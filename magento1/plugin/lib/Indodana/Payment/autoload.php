781d49d075824cf865b59364895b3c956816c585
Steven Yang <stv.yang10@gmail.com>
2020-03-05 22:40:54 +0700

    [T22390] [Magento1] Integrate Sentry
    Summary: None
    
    Test Plan: Skip Test
    
    Reviewers: #project_athena, sendy
    
    Reviewed By: #project_athena, sendy
    
    Maniphest Tasks: T22390
    
    Differential Revision: https://p.cermati.com/D29520

--- magento1/plugin/lib/Indodana/Payment/autoload.php
+++ magento1/plugin/lib/Indodana/Payment/autoload.php
@@ -7,6 +7,14 @@ define('INDODANA_LIB_DIR', __DIR__ . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEP
 // Required for IndodanaLogger
 define('INDODANA_LOG_DIR', __DIR__ . DIRECTORY_SEPARATOR . 'log' . DIRECTORY_SEPARATOR);
 
+// Initialize Sentry
+$client = new Raven_Client('https://1c802ead2db94064aea8611b55cf536f@sentry.io/3073180');
+
+$error_handler = new Raven_ErrorHandler($client);
+$error_handler->registerExceptionHandler();
+$error_handler->registerErrorHandler();
+$error_handler->registerShutdownFunction();
+
 spl_autoload_register(function($className) {
     static $classMap;
 
