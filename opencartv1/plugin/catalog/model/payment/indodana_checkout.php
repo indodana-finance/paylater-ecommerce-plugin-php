da977fde8cb2a9df8500fc90b8c1ebaffc60164c
Steven Yang <stv.yang10@gmail.com>
2020-04-16 18:06:35 +0700

    feat: Use internally hosted logo on all plugins

--- opencartv1/plugin/catalog/model/payment/indodana_checkout.php
+++ opencartv1/plugin/catalog/model/payment/indodana_checkout.php
@@ -1,23 +1,29 @@
 <?php
+
+require_once DIR_SYSTEM . 'library/indodana/autoload.php';
+
+use IndodanaCommon\IndodanaConstant;
+
 class ModelPaymentIndodanaCheckout extends Model {
-    const MINIMIM_ORDER_AMOUNT = 10000;
+  const MINIMIM_ORDER_AMOUNT = 10000;
 
-    public function getMethod($address, $total) {
-        $defaultCurrency = $this->config->get('config_currency');
-        $totalInIDR = ceil($this->currency->convert($total, $defaultCurrency, 'IDR'));
+  public function getMethod($address, $total) {
+    $defaultCurrency = $this->config->get('config_currency');
+    $totalInIDR = ceil($this->currency->convert($total, $defaultCurrency, 'IDR'));
 
-        if ($totalInIDR < self::MINIMIM_ORDER_AMOUNT) {
-            return null;
-        }
-
-        $method_data = array(
-          'code'     => 'indodana_checkout',
-          'title'    => '<img src="https://afpi.or.id/fm/Members/indodana_logo_4500-x-1000.png" height="25" width="112">',
-          'sort_order' => $this->config->get('indodana_checkout_sort_order')
-        );
-
-        return $method_data;
+    if ($totalInIDR < self::MINIMIM_ORDER_AMOUNT) {
+      return null;
     }
+
+    $logoUrl = IndodanaConstant::LOGO_URL;
+    $method_data = array(
+      'code'     => 'indodana_checkout',
+      'title'       => "<img src='${logoUrl}' height='25' width='112'>",
+      'sort_order' => $this->config->get('indodana_checkout_sort_order')
+    );
+
+    return $method_data;
+  }
 
   private function getOrderTotalRows($order_id, $codes)
   {
--- opencartv2/plugin/admin/language/english/payment/indodana_checkout.php
+++ opencartv2/plugin/admin/language/english/payment/indodana_checkout.php
@@ -9,4 +9,4 @@ $_['button_cancel'] = 'Cancel';
 $_['entry_status'] = 'Status';
 $_['entry_sort_order'] = 'Sort Order';
 
-$_['text_indodana_checkout'] = '<a onclick="window.open(\'http://indodana.com\');"><img src="https://afpi.or.id/fm/Members/indodana_logo_4500-x-1000.png" alt="Indodana Payments" title="Indodana Payments" style="border: 1px solid #EEEEEE;" height="25" width="112"/></a>';
+$_['text_indodana_checkout'] = '<a onclick="window.open(\'http://indodana.com\');"><img src="https://indodana.imgix.net/logo-color.png" alt="Indodana Payments" title="Indodana Payments" style="border: 1px solid #EEEEEE;" height="25" width="112" /></a>';
