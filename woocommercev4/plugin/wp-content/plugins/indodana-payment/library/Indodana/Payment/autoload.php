acfc0f707023101d8286f9d4be9fb8a6932fc455
Steven Yang <stv.yang10@gmail.com>
2020-07-16 12:57:26 +0700

    feat: Remove error tracking feature to improve performance

--- woocommercev4/plugin/wp-content/plugins/indodana-payment/library/Indodana/Payment/autoload.php
+++ woocommercev4/plugin/wp-content/plugins/indodana-payment/library/Indodana/Payment/autoload.php
@@ -6,10 +6,3 @@ define('INDODANA_PLUGIN_ROOT_DIR', __DIR__ . DIRECTORY_SEPARATOR);
 
 // Required for IndodanaLogger
 define('INDODANA_LOG_DIR', __DIR__ . DIRECTORY_SEPARATOR . 'log' . DIRECTORY_SEPARATOR);
-
-// Initialize Sentry
-use IndodanaCommon\IndodanaCommon;
-
-$sentryDsn = IndodanaCommon::getSentryDsn('WOOCOMMERCEV4');
-
-Sentry\init(['dsn' => $sentryDsn ]);
