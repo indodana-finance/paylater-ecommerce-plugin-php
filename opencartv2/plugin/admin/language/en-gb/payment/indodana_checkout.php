6762ed0f9702fe1819ea73b5c51614efb229fa27
Steven Yang <stv.yang10@gmail.com>
2020-10-21 00:08:21 +0700

    feat(opencartv2): Add en-gb language for Opencart >= 2.2.0.0 support

---
+++ opencartv2/plugin/admin/language/en-gb/payment/indodana_checkout.php
@@ -0,0 +1,13 @@
+<?php
+$_['heading_title'] = 'Indodana Paylater';
+$_['subheading_title'] = 'Edit Configuration';
+$_['text_success'] = 'Success';
+
+$_['button_save'] = 'Save';
+$_['button_cancel'] = 'Cancel';
+
+$_['entry_status'] = 'Status';
+$_['entry_sort_order'] = 'Sort Order';
+
+$_['text_indodana_checkout'] = '<a onclick="window.open(\'http://indodana.com\');"><img src="https://indodana-web.imgix.net/assets/indodana-new-color.png?width=150&height=50" alt="Indodana Payments" title="Indodana Payments" style="border: 1px solid #EEEEEE;"/></a>';
+
